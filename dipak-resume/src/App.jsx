42f13f4d869b95ecacafb17265a263dd48efe51f
dipak02 <shahdipak5654@gmail.com>
2025-11-19 18:00:48 +0545

    now all set and it is running on the local host perfectly

--- dipak-resume/src/App.jsx
+++ dipak-resume/src/App.jsx
@@ -2,16 +2,42 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/Navbar.jsx';
+
+
+import Navbar from './components/Navbar'
+import Main from './components/Main'
+import Tools from './components/Tools'
+import Services from './components/Services'
+import Project from './components/Project'
+import Blog from './components/Blog'
+import Contact from './components/Contact'
+import Faqs from './components/Faqs'
+import Testimonials from './components/Testimonials'
+import Footer from './components/Footer'
+import Academic from './components/Academic'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
+     <div id="home" class="w-full mx-auto p-6 space-y-6">
       <Navbar />
+      <Main/>
+      <Tools/>
+      <Services/>
+      <Academic/>
+      <Project/>
+      <Contact/>
+      <Testimonials/>
+      <Blog/>
+      <Faqs/>
+      <Footer/>
+      </div>
     </>
   )
 }
+
+
 
 export default App
